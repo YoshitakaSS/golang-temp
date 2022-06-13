467046f9ff591a5af7e4fc3898c93bfc64692351
ys <yoshi.23unog34@gmail.com>
2022-06-13 16:15:29 +0900

    Update: main.go

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
@@ -10,7 +11,9 @@ func main() {
 	e := echo.New()
 
 	e.GET("/health", func(c echo.Context) error {
+		fmt.Print("test")
 		return c.NoContent(http.StatusOK)
 	})
 
+	e.Start(":8080")
 }
